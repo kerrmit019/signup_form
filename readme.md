26efae1632d6eca09cca554ac09e1c836b272acc
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-04-18 21:06:09 +1200

    Finish sign-up form project

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 #Sign Up Form for The Odin Project
+Basic Form Styling - form goes nowhere
+Basic form validation
--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("This is a test");
+// add in error message
+// with bad password matching criteria for demo purpose only
+// there is no authentication
+
+const passwordField = document.querySelector("#password");
+const confirmPasswordField = document.querySelector("#confirmPassword");
+const errorMessage = document.querySelector(".error-message");
+
+// event listeners.
+
+passwordField.addEventListener("input", checkPasswordsMatch);
+confirmPasswordField.addEventListener("input", checkPasswordsMatch);
+
+function checkPasswordsMatch() {
+  console.log(passwordField.value);
+  if (passwordField.value === confirmPasswordField.value) {
+    passwordField.classList.remove("error");
+    confirmPasswordField.classList.remove("error");
+    errorMessage.style.visibility = "hidden";
+    return;
+  }
+  passwordField.classList.add("error");
+  errorMessage.style.visibility = "visible";
+  confirmPasswordField.classList.add("error");
+  return;
+}
